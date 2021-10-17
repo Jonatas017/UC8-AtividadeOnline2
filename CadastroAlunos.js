1aff7d3406edc9b9d90694240fd028bee60eeb93
Jonatas Nascimento <jts.nascimento17@hotmail.com>
2021-10-17 11:00:17 -0300

    AtividadeOnline2 - Aplicando o fluxograma

---
+++ CadastroAlunos.js
@@ -0,0 +1,36 @@
+//dia do evento
+let diaAtual = 05/01/2021
+
+if (diaAtual <= 06/01/2021) {
+    console.log("permitido, continuar cadastro")
+ } else{
+    console.log("não permitido, data inválida.")
+
+}
+
+//idade do participante
+let idade = 18
+
+if (idade <= 19) {
+    console.log ("permitido, continuar cadastro")
+    
+} else {
+    console.log("cadastro não é permitido pela idade.")
+}
+
+//lista de participantes/palestrantes
+let listaTotal = 99
+
+for (let listaTotal = 0; listaTotal < 99; listaTotal++) {
+    console.log("Participante/Palestrante " + listaTotal)
+    
+}
+
+if (listaTotal >= 100) {
+    console.log("permitido, continuar cadastro")
+}else 
+{   console.log("cadastro não será permitido por ter excedido o limite.")
+
+    
+}
+
